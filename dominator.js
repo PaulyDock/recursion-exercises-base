a3b45c3544c27eef094ea8fadebf1e254f819e21
Paul Dockery <paul.david.dockery@gmail.com>
2017-05-12 14:42:35 -0700

    Add getElementById() coding

--- dominator.js
+++ dominator.js
@@ -2,7 +2,21 @@ const { flattenTreeToArray } = require('./dom-util');
 const _ = require('underscore');
 
 const getElementById = function(root, id) {
-  // Your code here
+  var flatNodes = flattenTreeToArray(root);
+  //console.log(flatNodes);
+  flatNodes = _.reject(flatNodes, (node) => node.nodeType === 3);
+  //console.log(flatNodes);
+  flatNodes = _.filter(flatNodes, (node) => node.id === id);
+  //console.log(flatNodes);
+  return flatNodes[0];
+
+//  flatNodes = _.map(flatNodes, (node) => node.parentNode);
+//  console.log(flatNodes);
+
+//  for (let i = 0; i < 20; i++) {
+//    let curNode = flatNodes[i];
+//    console.log(curNode.tagName, curNode.src);
+//  }
 };
 
 const getElementsByClassName = function(root, className) {
