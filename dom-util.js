5e288b6d1c1d7c0a55a2914039e778adacb6826a
Paul Dockery <paul.david.dockery@gmail.com>
2017-05-12 11:22:21 -0700

    Consolidate visitAllNodes line in flattenTreeToArray()

--- dom-util.js
+++ dom-util.js
@@ -1,3 +1,7 @@
+const fs = require('fs');
+const data = fs.readFileSync('./test/fixtures/broccoli.json', 'utf-8');
+const broccoli = JSON.parse(data);
+
 const visitAllNodes = function(node, callback) {
   if (node.childNodes) {
     let children = node.childNodes;
@@ -10,9 +14,7 @@ const visitAllNodes = function(node, callback) {
 
 const flattenTreeToArray = function(parentNode) {
   const result = [];
-  visitAllNodes(parentNode, (node) => {
-    result.push(node);
-  });
+  visitAllNodes(parentNode, (node) => result.push(node));
   return result;
 };
 
