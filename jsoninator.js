b0d774711be7e7f7f1292364657e5553d3a07a35
Paul Dockery <paul.david.dockery@gmail.com>
2017-05-12 12:56:30 -0700

    Add some useful structures for troubleshooting to jsoninator

--- jsoninator.js
+++ jsoninator.js
@@ -4,11 +4,32 @@ const _ = require('underscore'); // the real one! :)
 // const stringify = JSON.stringify;
 // But you don't. So you're going to write it from scratch...
 
-
+const testCases = [
+9,
+null,
+true,
+false,
+'Hello world',
+[],
+[8],
+[8, 'hi'],
+[1, 0, -1, -0.3, 0.3, 1343.32, 3345, 0.00011999999999999999],
+[8, [[], 3, 4]],
+[[[['foo']]]],
+{},
+{'a': 'apple'},
+[{'a': 'b'}, {'c': 'd'}],
+{'a': ['b', 'c']}
+]
 
 const stringify = function(obj) {
-  // your code goes here
+  if (Object.prototype.toString.call(obj) === '[object Array]') {
+    return true;
+  }
+  return false;
 };
+
+_.each(testCases, (entry) => console.log(entry, "-", stringify(entry)));
 
 module.exports = {
   stringify: stringify
