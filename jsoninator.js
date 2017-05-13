8dde4d97e3a72c54c62e79046c3ea6e3eeb630cd
Paul Dockery <paul.david.dockery@gmail.com>
2017-05-12 19:32:18 -0700

    Remove much troubleshooting/developer commentary from jsoninator

--- jsoninator.js
+++ jsoninator.js
@@ -1,35 +1,10 @@
 const _ = require('underscore'); // the real one! :)
 
-// This is what you would do if you liked things to be easy:
-// const stringify = JSON.stringify;
-// But you don't. So you're going to write it from scratch...
-
-const testCases = [
-9,
-null,
-true,
-false,
-'Hello world',
-[],
-[8],
-[8, 'hi'],
-[1, 0, -1, -0.3, 0.3, 1343.32, 3345, 0.00011999999999999999],
-[8, [[], 3, 4]],
-[[[['foo']]]],
-{},
-{'a': 'apple'},
-[{'a': 'b'}, {'c': 'd'}],
-{'a': ['b', 'c']}
-];
-
-
 const stringify = function(obj) {
   var stringifiedObj = '';
 
   switch (Object.prototype.toString.call(obj)) {
-    
     case '[object Number]':
-      //return obj;
       stringifiedObj += obj;
       break;
 
@@ -39,7 +14,6 @@ const stringify = function(obj) {
     
     case '[object Boolean]':
       stringifiedObj += `${obj}`;
-//      return `\"${obj}\"`;
       break;
 
     case '[object String]':
@@ -54,29 +28,20 @@ const stringify = function(obj) {
         stringedObj += `${stringifiedKey}:${stringifiedPropValue}`;
       });
       stringifiedObj += '{' + stringedObj + '}';
-//      return '{ ' + stringedObj + ' }';
       break;
 
     case '[object Array]':
       resArr = _.map(obj, (entry) => stringify(entry));
       stringifiedObj += '[' + `${resArr}` + ']';
-//      return '[' + `${resArr}` + ']';
       break;
 
     default:
-//      return `"${obj}"`;
       return obj;
       break;
-
   }
 
   return stringifiedObj;
 };
-
-//console.log(testCases.hasChildNodes());
-//_.each(testCases, (entry) => console.log(entry, '-', entry.hasChildNodes()));
-//_.each(testCases, (entry) => console.log(entry, '-', Object.prototype.toString.call(entry)));
-//_.each(testCases, (entry) => console.log(entry, "-", stringify(entry)));
 
 module.exports = {
   stringify: stringify
