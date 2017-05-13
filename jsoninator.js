ec79775d4c9b11e04cc848d8f543b0dc1e51878f
Paul Dockery <paul.david.dockery@gmail.com>
2017-05-12 19:26:54 -0700

    Add jsoninator coding

--- jsoninator.js
+++ jsoninator.js
@@ -20,16 +20,63 @@ false,
 {'a': 'apple'},
 [{'a': 'b'}, {'c': 'd'}],
 {'a': ['b', 'c']}
-]
+];
+
 
 const stringify = function(obj) {
-  if (Object.prototype.toString.call(obj) === '[object Array]') {
-    return true;
+  var stringifiedObj = '';
+
+  switch (Object.prototype.toString.call(obj)) {
+    
+    case '[object Number]':
+      //return obj;
+      stringifiedObj += obj;
+      break;
+
+    case '[object Null]':
+      stringifiedObj += `${obj}`;
+      break;
+    
+    case '[object Boolean]':
+      stringifiedObj += `${obj}`;
+//      return `\"${obj}\"`;
+      break;
+
+    case '[object String]':
+      stringifiedObj += '\"' + obj + '\"';
+      break;
+
+    case '[object Object]':
+      let stringedObj = '';
+      _.each(obj, (value, key) => {
+        stringifiedPropValue = stringify(value);
+        stringifiedKey = stringify(key);
+        stringedObj += `${stringifiedKey}:${stringifiedPropValue}`;
+      });
+      stringifiedObj += '{' + stringedObj + '}';
+//      return '{ ' + stringedObj + ' }';
+      break;
+
+    case '[object Array]':
+      resArr = _.map(obj, (entry) => stringify(entry));
+      stringifiedObj += '[' + `${resArr}` + ']';
+//      return '[' + `${resArr}` + ']';
+      break;
+
+    default:
+//      return `"${obj}"`;
+      return obj;
+      break;
+
   }
-  return false;
+
+  return stringifiedObj;
 };
 
-_.each(testCases, (entry) => console.log(entry, "-", stringify(entry)));
+//console.log(testCases.hasChildNodes());
+//_.each(testCases, (entry) => console.log(entry, '-', entry.hasChildNodes()));
+//_.each(testCases, (entry) => console.log(entry, '-', Object.prototype.toString.call(entry)));
+//_.each(testCases, (entry) => console.log(entry, "-", stringify(entry)));
 
 module.exports = {
   stringify: stringify
