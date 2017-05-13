92fefece6e2660862ae4d83379ccd997b822be61
Paul Dockery <paul.david.dockery@gmail.com>
2017-05-12 17:03:06 -0700

    Remove troubleshooting comments from dominator.js

--- dominator.js
+++ dominator.js
@@ -3,45 +3,19 @@ const _ = require('underscore');
 
 const getElementById = function(root, id) {
   var flatNodes = flattenTreeToArray(root);
-  //console.log(flatNodes);
   flatNodes = _.reject(flatNodes, (node) => node.nodeType === 3);
-  //console.log(flatNodes);
   flatNodes = _.filter(flatNodes, (node) => node.id === id);
-  //console.log(flatNodes);
   return flatNodes[0];
-
-//  flatNodes = _.map(flatNodes, (node) => node.parentNode);
-//  console.log(flatNodes);
-
-//  for (let i = 0; i < 20; i++) {
-//    let curNode = flatNodes[i];
-//    console.log(curNode.tagName, curNode.src);
-//  }
 };
 
 const getElementsByClassName = function(root, className) {
   var flatNodes = flattenTreeToArray(root);
   flatNodes = _.reject(flatNodes, (node) => node.nodeType === 3);
-  //console.log(flatNodes);
   flatNodes = _.filter(flatNodes, (node) => {
     let attributes = node.attributes;
     return _.some(attributes, (attribute) => _.indexOf(attribute.value.split(' '), className) > -1);
   });
   return flatNodes;
-  
-  /*_.each(flatNodes, (node) => {
-    let attributes = node.attributes;
-    console.log("----", node, attributes, "----");
-    _.each(attributes, (attribute) => {
-      //console.log(attribute.name);
-      console.log(attribute.value);
-    });
-  });*/
-  //console.log(flatNodes);
-
-/*  flatNodes = _.filter(flatNodes, (node) => node.class === className);
-  console.log(flatNodes);
-  return flatNodes;*/
 };
 
 const getElementsByTagName = function(root, tagName) {
